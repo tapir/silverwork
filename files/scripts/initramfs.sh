d6217e672840c4ea226d7a632099a51fccffc30e
Cosku Bas <cosku.bas@gmail.com>
2026-01-18 13:18:52 +0100

    use recipes instead og bash

--- files/scripts/initramfs.sh
+++ files/scripts/initramfs.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-set -ouex pipefail
+set -oue pipefail
 
 KERNEL_VERSION="$(rpm -q --queryformat="%{evr}.%{arch}" kernel-cachyos-lto)"
-
-# Ensure Initramfs is generated
 export DRACUT_NO_XATTR=1
+
 /usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
 chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
